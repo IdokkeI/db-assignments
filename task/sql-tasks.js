6e04d0cf3e890d70b1d7baa31b1ff84c6562ffbd
d1moza <gumbit9@mail.ru>
2020-09-12 14:47:17 +0300

    fix

--- task/sql-tasks.js
+++ task/sql-tasks.js
@@ -24,13 +24,13 @@ async function task_1_1(db) {
     // The first task is example, please follow the style in the next functions.
     debugger;
     let result = await db.query(`
-        SELECT
-           EmployeeID as "Employee Id",
-           CONCAT(FirstName, ' ', LastName) AS "Employee Full Name",
-           Title as "Title",
-           City as "City"
-        FROM Employees
-            Order BY City, "Employee Full Name"
+    SELECT
+        EmployeeID as "Employee Id",
+        CONCAT(FirstName, ' ', LastName) AS "Employee Full Name",
+        Title as "Title",
+        City as "City"
+    FROM Employees
+        Order BY City, "Employee Full Name"
     `);
     return result[0];
 }
@@ -51,8 +51,8 @@ async function task_1_2(db) {
         SUM(UnitPrice * Quantity) as "Order Total Price",
         ROUND(SUM(Discount * Quantity)/SUM(UnitPrice * Quantity) * 100,3) as "Total Order Discount, %"    
     FROM OrderDetails
-        GROUP BY OrderID
-        Order BY OrderID desc
+    GROUP BY OrderID
+    ORDER BY OrderID desc
     `);
     return result[0];
 }
@@ -90,11 +90,11 @@ async function task_1_4(db) {
     let result = await db.query(`
     SELECT 
         CustomerID as "Customer Id",
-        count(CustomerID) as "Total number of Orders",
-        round(count(*) / SUM(count(*)) over() * 100, 5) as "% of all Orders"    
+        COUNT(CustomerID) as "Total number of orders",
+        ROUND(count(*) / SUM(count(*)) OVER() * 100, 5) as "% of all orders"    
     FROM Orders
-        GROUP BY CustomerID
-        ORDER BY 3 desc, CustomerID asc
+    GROUP BY CustomerID
+    ORDER BY 3 desc, CustomerID asc
     `);
     return result[0];
 }
@@ -114,7 +114,7 @@ async function task_1_5(db) {
         QuantityPerUnit as "QuantityPerUnit"
     FROM Products
         WHERE ProductName between "A" and "G"
-        ORDER BY ProductName
+    ORDER BY ProductName
     `);
     return result[0];
 }
@@ -137,7 +137,7 @@ async function task_1_6(db) {
     FROM Products 
        INNER JOIN Categories on Products.CategoryID = Categories.CategoryID
        INNER JOIN Suppliers on Products.SupplierID = Suppliers.SupplierID
-       ORDER BY 1, 3
+    ORDER BY 1, 3
     
     `);
     return result[0];
@@ -155,13 +155,13 @@ async function task_1_6(db) {
  */
 async function task_1_7(db) {
     let result = await db.query(`
-    SELECT 
-        e1.EmployeeId as "EmployeeId",
-        CONCAT( e1.FirstName, " ", e1.LastName) as "FullName",
-        ifnull(CONCAT( e2.FirstName, " ", e2.LastName), '-')  as "ReportsTo"
-    FROM Employees e1
-        LEFT OUTER JOIN Employee e2 on e2.EmployeeID = e1.ReportsTo
-        ORDER BY e2.EmployeeID
+    SELECT  
+	    e.EmployeeID as 'EmployeeId',
+	    CONCAT(e.FirstName,' ',e.LastName) as "FullName",
+	    IFNULL(CONCAT(r.FirstName,' ',r.LastName),'-') as "ReportsTo"
+    FROM Employees e
+        LEFT OUTER JOIN Employees r ON r.EmployeeID = e.ReportsTo
+    ORDER BY e.EmployeeID
     `);
     return result[0];
 }
@@ -181,8 +181,8 @@ async function task_1_8(db) {
         count(p.CategoryID) as "TotalNumberOfProducts"
     FROM Products p 
         INNER JOIN Categories c	on p.CategoryID = c.CategoryID
-        GROUP BY c.CategoryName
-        ORDER BY 1
+    GROUP BY c.CategoryName
+    ORDER BY 1
     `);
     return result[0];
 }
@@ -296,7 +296,7 @@ async function task_1_14(db) {
         UnitsOnOrder,
         UnitsInStock
     FROM Products
-	WHERE UnitsInStock < UnitsOnOrder
+	    WHERE UnitsInStock < UnitsOnOrder
     `);
     return result[0];
 }
@@ -324,7 +324,7 @@ async function task_1_15(db) {
         COUNT(IF(MONTHNAME(OrderDate)="November",1,Null)) as "November",
 	    COUNT(IF(MONTHNAME(OrderDate)="December",1,Null)) as "December"
     FROM Orders 
-    WHERE YEAR(OrderDate)='1997'
+        WHERE YEAR(OrderDate)='1997'
     ORDER BY OrderDate
     `);
     return result[0];
@@ -344,7 +344,7 @@ async function task_1_16(db) {
         CustomerID,
         ShipCountry
     FROM Orders
-	WHERE ShipPostalCode is not null
+	    WHERE ShipPostalCode is not null
     `);
     return result[0];
 }
@@ -364,8 +364,8 @@ async function task_1_17(db) {
 		avg(p.UnitPrice) as "AvgPrice"
     FROM Products p
         INNER JOIN Categories c on c.CategoryID = p.CategoryID
-        GROUP BY 1
-        Order BY 2 desc, 1
+    GROUP BY 1
+    ORDER BY 2 desc, 1
         `);
         return result[0];
 }
@@ -388,7 +388,7 @@ async function task_1_18(db) {
         WHERE YEAR(OrderDate) = 1998 and 
         MONTH(OrderDate) between 1 and 12 and 
         DAY(OrderDate) between 1 and 31) T
-        GROUP BY 1
+    GROUP BY 1
         `);
         return result[0];
 }
@@ -410,9 +410,9 @@ async function task_1_19(db) {
     FROM Orders o
         INNER JOIN Customers c on c.CustomerID = o.CustomerID
         INNER JOIN OrderDetails od on od.OrderID = o.OrderID
-        GROUP BY 1
-        HAVING SUM(od.Quantity * od.UnitPrice) > 10000
-        Order BY 3 desc, 1 desc
+    GROUP BY 1
+    HAVING SUM(od.Quantity * od.UnitPrice) > 10000
+    Order BY 3 desc, 1 desc
         `);
         return result[0];
 }
